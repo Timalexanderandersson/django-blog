350f9e611fae32bb4fd4f032d1492776aa04ef8f
Tim Andersson <timalexanderandersson@hotmail.com>
2024-08-26 13:00:42 +0000

    .

--- blog/views.py
+++ blog/views.py
@@ -80,7 +80,6 @@ def comment_edit(request, slug, comment_id):
 
     return HttpResponseRedirect(reverse('post_detail', args=[slug]))
 
-
 def comment_delete(request, slug, comment_id):
     """
     view to delete comment
